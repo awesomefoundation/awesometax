5d38df6cf42b72bbb008fd1489001da0daeae9f2
Larry Sheradon <sheradon@gmail.com>
2012-01-07 00:45:09 +0000

    staging

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Rails.env = 'staging'
 
 ActionMailer::Base.smtp_settings = {
   :user_name            => "lsheradon",
