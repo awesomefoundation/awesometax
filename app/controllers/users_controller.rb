dac44f88d00aa3be70aa522744b83fee37d4ebd6
Larry Sheradon <sheradon@gmail.com>
2012-01-09 23:31:42 +0000

    my-profile and admin cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,17 @@ class UsersController < ApplicationController
     #@funded = @user.funded_taxes.order('status, created_at desc')
     
     if @myself
-      @pledges = @user.pledges.order('status, created_at desc')
-      @total_given = @user.transactions.sent.sum(:amount)
-      @active_sum = @user.pledges.active.sum(:amount)
-      @active_count = @user.pledges.active.count
-      @transactions = @user.transactions.sort { |a,b| b.id <=> a.id }
+      @pledges        = @user.pledges.order('status, created_at desc')
+      @active_sum     = @user.pledges.active.sum(:amount)
+      @active_count   = @user.pledges.active.count
+      
+      managed_pledges = @managed.collect { |t| t.pledges.active }.flatten
+      @managed_count  = managed_pledges.count
+      @managed_sum    = managed_pledges.inject(0) { |sum,i| sum + i.amount }
+      
+      @total_given    = @user.transactions.sent.sum(:amount)
+      @total_received = @user.transactions.received.sum(:amount)
+      @transactions   = @user.transactions.sort { |a,b| b.id <=> a.id }
     else
       @pledges = @user.pledges.active.order('created_at desc')
     end
