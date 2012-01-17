0b098196f66f636b129b20fe256995c72e08626a
Larry Sheradon <sheradon@gmail.com>
2012-01-17 06:57:11 +0000

    invite peeps to manage stuff

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,13 +6,36 @@ class AdminController < ApplicationController
     @taxes = Tax.all
     @transactions = Transaction.all(:order => 'id desc')
     @comments = Comment.all(:order => 'id desc')
+    
+    invitable = admin? ? Tax.all : current_user.managed_taxes
+    @tax_invite_options = invitable.collect { |t| [ t.name, t.id ] }
   end
   
   def invite
     redirect_to admin_path, :notice => "You need to enter an email and name." and return if params[:email].blank? or params[:name].blank?
-    User.invite!({:email => params[:email], :name => params[:name]}, current_user)
-    invited = User.last.update_attribute(:status, User::TRUSTEE)
-    redirect_to admin_path, :notice => "Sent invitation to #{params[:email]}."
+    
+    if params[:tax_id]
+      tax = Tax.find params[:tax_id]
+      redirect_to admin_path, :notice => "You can't invite to that tax." and return unless admin? or tax.managers.include? current_user
+    else
+      tax = nil
+    end
+    
+    if exists = User.find_by_email(params[:email])
+      if tax 
+        tax.managers << exists
+        redirect_to admin_path, :notice => "Added #{params[:email]}."
+      else
+        redirect_to admin_path, :notice => "That email is already registered!"
+      end
+    else
+      User.invite!({:email => params[:email], :name => params[:name]}, current_user)
+      invited = User.last
+      invited.update_attribute(:status, User::TRUSTEE)
+      redirect_to admin_path, :notice => "Sent invitation to #{params[:email]}."
+      tax.managers << invited
+    end
+    
   end
   
 end
