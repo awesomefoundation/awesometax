ceaddcbb8f45d9c51d8a9359bbb4f9b1ec47fd82
Larry Sheradon <sheradon@gmail.com>
2012-02-09 21:53:46 +0000

    gah

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,6 @@
 LoveTax3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  puts "I'm in staging config"
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
