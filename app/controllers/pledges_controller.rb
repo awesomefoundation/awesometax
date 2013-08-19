78d6f16bd80d6031b4c5557398bf84bff5292540
Kate Ray <email@kateray.net>
2013-08-19 14:57:44 -0400

    fixes #4

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,13 +1,16 @@
 class PledgesController < ApplicationController
 
-  before_filter :authenticate_user!, :only => [:create, :update, :destroy, :start, :pause, :stop, :collect]
+  before_filter :store_pledge_amount, :only => :create
+  before_filter :authenticate_user!, :only => [:update, :destroy, :start, :pause, :stop, :collect]
   protect_from_forgery :except => :notify # IPN gets invalid auth token messages otherwise
 
   def show
     @pledge = Pledge.find params[:id]
   end
-  
+
   def create
+    session[:pledge_amount] = nil
+
     @pledge = Pledge.new(params[:pledge])
     @pledge.attributes = {
       :user => current_user,
@@ -19,7 +22,7 @@ class PledgesController < ApplicationController
       redirect_to @pledge.tax, :notice => "That wasn't allowed, sorry."
       return
     end
-    
+
     pay_request = PaypalAdaptive::Request.new
     receiverList = [ { 'email' => @pledge.tax.paypal_email, 'amount' => @pledge.recipient_cut.to_s } ]
     receiverList << { 'email' => AppConfig.paypal_email, 'amount' => @pledge.loveland_cut.to_s } if AppConfig.loveland_fee > 0
@@ -40,7 +43,7 @@ class PledgesController < ApplicationController
       'endingDate'                  => @pledge.ends.strftime('%Y-%m-%dT%H:%M:%S-00:00')
     }
     pay_response = @response = pay_request.preapproval(data)
-    
+
     if pay_response.success?
       @pledge.update_attribute(:preapproval_key, pay_response['preapprovalKey'])
       redirect_to pay_response.preapproval_paypal_payment_url
@@ -49,8 +52,8 @@ class PledgesController < ApplicationController
       redirect_to @pledge.tax
     end
   end
-  
-  
+
+
   def completed
     pledge = Pledge.find params[:id]
     pledge.user ||= current_user
@@ -73,26 +76,26 @@ class PledgesController < ApplicationController
       redirect_to pledge.tax
       return
     end
-    
+
     pledge.update_attribute(:status, Pledge::ACTIVE)
     pledge.tax.funders << current_user
-    
+
     begin
       Mailer.new_pledge(pledge.tax.managers.select { |u| u.settings['email.new_pledge'] }, pledge).deliver
     rescue => e
       logger.info e.inspect
       logger.info e.backtrace
     end
-    
+
     flash[:notice] = 'Thank you for pledging!'
     #redirect_to :action => 'thanks', :id => pledge.id
     redirect_to pledge.tax
   end
-  
+
   def thanks
     @pledge = Pledge.find params[:id]
   end
-  
+
   def canceled
     pledge = Pledge.find params[:id]
     if pledge.user == current_user
@@ -101,13 +104,13 @@ class PledgesController < ApplicationController
     flash[:notice] = 'Canceled your payment.'
     redirect_to pledge.tax
   end
-  
+
   # IPN, we don't have to do anything
   def notify
     render :text => 'Ok, thank you.'
   end
-  
-  
+
+
   def start
     @pledge = Pledge.find params[:id]
     redirect_to account_path
@@ -129,8 +132,8 @@ class PledgesController < ApplicationController
     current_user.funded_taxes.delete(@pledge.tax)
     @pledge.stop
   end
-  
-  
+
+
   # Force collection right now of your own pending pledges. This is for testing!
   def collect
     redirect_to history_path
@@ -138,10 +141,19 @@ class PledgesController < ApplicationController
       flash[:notice] = "This feature's for testing only."
       return
     end
-    
+
     current_user.pledges.active.each do |p|
       p.collect
     end
   end
-  
+
+  private
+
+  def store_pledge_amount
+    unless user_signed_in?
+      session[:pledge_amount] = params[:pledge][:amount]
+      redirect_to new_user_registration_path
+    end
+  end
+
 end
