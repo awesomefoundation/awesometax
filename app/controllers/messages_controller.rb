378507d3ac9a2904842acd51dc3b79ba8716e1bf
Kate Ray <email@kateray.net>
2013-08-27 16:55:54 -0700

    only admin and managers can send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,51 +1,62 @@
 class MessagesController < ApplicationController
-  before_filter :require_admin_or_trustee
   helper_method :tax_options
-  
+
   def new
+    unless can_edit?(params[:tax_id])
+      redirect_to account_path and return
+    end
+
     @message = Message.new(:tax_id => params[:tax_id])
   end
-  
+
   def create
     @message = Message.new(params[:message])
-    @message.user = current_user
-    unless @message.tax.andand.managers.include? current_user
-      redirect_to account_path, :notice => "You don't manage that tax, sorry."
-      return
+
+    unless can_edit?(@message.tax_id)
+      redirect_to account_path and return
     end
+
+    @message.user = current_user
+
     if @message.save
       redirect_to account_path(:anchor => 'messages'), :notice => @message.published? ? "Successfully published and sent your message!" : "Saved your message as a draft."
     else
       render :action => 'new', :notice => "There was a problem and your mail wasn't saved."
     end
   end
-  
+
   def edit
     @message = Message.find params[:id]
+
+    unless can_edit?(@message.tax_id)
+      redirect_to account_path and return
+    end
+
     redirect_to account_path(:anchor => 'messages'), :notice => "You don't have permission for that." unless @message.tax.managers.include? current_user
-  end  
+  end
 
   def update
     @message = Message.find params[:id]
-    redirect_to account_path(:anchor => 'messages'), :notice => "You don't have permission for that." unless @message.tax.managers.include? current_user
-    @message.attributes = params[:message]
-    unless @message.tax.andand.managers.include? current_user
-      redirect_to account_path(:anchor => 'messages'), :notice => "You don't have permission for that." and return
+    unless can_edit?(@message.tax_id)
+      redirect_to account_path and return
     end
+
+    @message.attributes = params[:message]
+
     if @message.save
       redirect_to account_path(:anchor => 'messages'), :notice => "Saved your changes to the message."
     else
       redirect_to account_path(:anchor => 'messages'), :notice => "There was a problem saving those changes, oops."
     end
   end
-  
+
   def destroy
   end
-  
+
   protected
-  
+
   def tax_options
     current_user.managed_taxes.collect { |t| [ t.name, t.id ] }
   end
-  
+
 end
