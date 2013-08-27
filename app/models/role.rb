8d379f6c2f30c755664b97a5c6802c50be2b594d
Kate Ray <email@kateray.net>
2013-08-27 14:33:26 -0700

    user cannot have multiple funder/manager roles per tax

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,11 @@
 class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :tax
-  
+
   # Kinds
   MANAGER   = 1
   FUNDER    = 2
   FAVORITE  = 3
+
+  validates :user_id, uniqueness: { :scope => [:tax_id, :kind]}
 end
