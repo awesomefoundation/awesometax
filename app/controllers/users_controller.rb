8b8a1902965fabc5b055ffc3185a7a6abf938af3
Kate Ray <email@kateray.net>
2013-08-28 14:26:31 -0700

    both managers and trustees can view partial tax info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,34 @@
 class UsersController < ApplicationController
-    
+
   def show
     @user = params[:id].nil? ? current_user : User.find(params[:id])
     redirect_to root_url and return if @user.nil?
     @myself = current_user.andand.id == @user.id
-    
+
     @managed = @user.managed_taxes.order('status, created_at desc')
+    @trusted = @user.trusted_taxes.order('status, created_at desc')
+    @supervised = @user.managed_taxes + @user.trusted_taxes
+
     #@funded = @user.funded_taxes.order('status, created_at desc')
-    
+
     if @myself
       @pledges        = @user.pledges.order('status, created_at desc').where('status > 0')
       @active_sum     = @user.pledges.active.sum(:amount)
       @active_count   = @user.pledges.active.count
-      
-      managed_pledges = @managed.collect { |t| t.pledges.active }.flatten
-      @managed_count  = managed_pledges.count
-      @managed_sum    = managed_pledges.inject(0) { |sum,i| sum + i.amount }
-      
+
+      supervised_pledges = @supervised.collect { |t| t.pledges.active }.flatten
+      @supervised_count  = supervised_pledges.count
+      @supervised_sum    = supervised_pledges.inject(0) { |sum,i| sum + i.amount }
+
       @total_given    = @user.transactions.sent.sum(:amount)
       @total_received = @user.transactions.received.sum(:amount)
       @transactions   = @user.transactions.sort { |a,b| b.id <=> a.id }
-      
+
       @messages       = @user.messages.published
       @sent           = @user.sent_messages
     else
       @pledges = @user.pledges.active.order('created_at desc')
     end
   end
-    
+
 end
