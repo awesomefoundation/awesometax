51383306ed7cda644239f251c7fa452825149e9f
Kate Ray <email@kateray.net>
2013-09-09 16:52:27 -0700

    validation to ensure user cannot be a manager and trustee of a tax

--- app/models/role.rb
+++ app/models/role.rb
@@ -9,16 +9,20 @@ class Role < ActiveRecord::Base
   FUNDER    = 2
   TRUSTEE  = 3
 
-  validates :user_id, uniqueness: { :scope => [:tax_id, :kind]}
+  validates_uniqueness_of :user_id, :scope => [:tax_id, :kind]
   validate :user_is_not_manager_and_trustee
+
+  scope :admin, where('kind != ?', Role::FUNDER)
 
   def user_name
     true
   end
 
   def user_is_not_manager_and_trustee
-    if User.find(self.user_id).managed_taxes.include?(self.tax)
-      errors.add(:user_id, " is already a manager")
+    unless kind == Role::FUNDER
+      if user.roles.admin.where('tax_id = ? && id != ?', tax_id, id).any?
+        errors.add(:user_id, " is already a trustee or manager")
+      end
     end
   end
 
