1787cb0e394133ccf70c315ecc933a6e0528fa6e
Kate Ray <email@kateray.net>
2013-09-11 16:11:15 -0700

    change devise migration for Devise 2.0

--- db/migrate/20111123050351_devise_create_users.rb
+++ db/migrate/20111123050351_devise_create_users.rb
@@ -1,18 +1,53 @@
 class DeviseCreateUsers < ActiveRecord::Migration
+  #update migration for Devise 2.0
+  #see https://github.com/plataformatec/devise/wiki/How-To:-Upgrade-to-Devise-2.0-migration-schema-style
   def self.up
     create_table(:users) do |t|
       t.string    :name,              :null => false
-      t.string    :email,             :null => false
       t.integer   :status
 
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      ## Database authenticatable
+      t.string :email,              :null => false
+      t.string :encrypted_password, :null => false
 
-      t.encryptable
-      t.confirmable
-      t.token_authenticatable
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+       ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Encryptable
+      t.string :password_salt
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
+      # Token authenticatable
+      t.string :authentication_token
+
+      # t.string    :name,              :null => false
+      # t.string    :email,             :null => false
+      # t.integer   :status
+
+      # t.database_authenticatable :null => false
+      # t.recoverable
+      # t.rememberable
+      # t.trackable
+
+      # t.encryptable
+      # t.confirmable
+      # t.token_authenticatable
 
       t.timestamps
     end
