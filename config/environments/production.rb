62d1e76cbbd137590938a2753ff81d2ebf94ac07
Kate Ray <email@kateray.net>
2013-10-08 14:22:47 -0400

    updated production.rb with mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,17 +43,18 @@ LoveTax3::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.default_url_options = { :host => 'awesometax-test.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'awesometax.awesomestudies.org'
+  Rails.application.routes.default_url_options[:host] = 'awesometax.herokuapp.com'
 
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.default_url_options = { :host => 'awesometax.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   # Enable threaded mode
   # config.threadsafe!
 
