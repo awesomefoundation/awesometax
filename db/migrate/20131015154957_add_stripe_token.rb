3eccecd62780d09e924de404236a58d0781fca2d
Kate Ray <email@kateray.net>
2013-10-16 14:35:24 -0400

    store stripe_token with pledges not users

--- db/migrate/20131015154957_add_stripe_token.rb
+++ db/migrate/20131015154957_add_stripe_token.rb
@@ -1,11 +1,13 @@
 class AddStripeToken < ActiveRecord::Migration
   def up
-    add_column :users, :stripe_token, :string
+    add_column :pledges, :stripe_token, :string
     add_column :taxes, :bank_token, :string
+    add_column :taxes, :recipient_id, :string
   end
 
   def down
-    remove_column :users, :stripe_token
+    remove_column :pledges, :stripe_token
     remove_column :taxes, :bank_token
+    remove_column :taxes, :recipient_id
   end
 end
