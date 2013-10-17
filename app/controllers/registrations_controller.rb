bf78fc4f903cd992a33aeefb35416c6c91260088
Kate Ray <email@kateray.net>
2013-10-17 13:50:50 -0400

    retain pending_pledge info after failed signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,29 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      if params[:pending_pledge_tax]
+        flash[:pending_pledge_tax] = params[:pending_pledge_tax]
+        flash[:pending_pledge_amount] = params[:pending_pledge_amount]
+      end
+      respond_with resource
+    end
+  end
+
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
 
