c907f40db7c7a1c8dc5dcd51b8b4c92f9c8ff3be
Kate Ray <email@kateray.net>
2013-12-12 13:51:41 -0500

    show transfers in admin view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,7 @@ class AdminController < ApplicationController
   def index
     @users = User.all(:limit => 25, :order => 'id desc')
     @taxes = Tax.all
+    @transfers = Transfer.all
     @transactions = Transaction.all(:order => 'id desc')
     @comments = Comment.all(:order => 'id desc')
   end
