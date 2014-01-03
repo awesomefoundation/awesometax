435f88f31d971d71d1b40b8d8059e213188cf415
Kate Ray <email@kateray.net>
2014-01-03 11:03:58 -0800

    temp remove safeguard

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -135,11 +135,11 @@ class Pledge < ActiveRecord::Base
   # This generally runs as a rake task, "rake taxes:collect"
   def self.collect_all
     # if (Time.new.day != 1) ||
-    if (Transaction.count == 0) ||
-      (Date.today.month == Transaction.last.created_at.month)
-      puts "There has already been a collection recently. No can do. This is a safeguard that's very easy to override if you mean it."
-      return
-    end
+    #   (Transaction.count == 0) ||
+    #   (Date.today.month == Transaction.last.created_at.month)
+    #   puts "There has already been a collection recently. No can do. This is a safeguard that's very easy to override if you mean it."
+    #   return
+    # end
 
     Tax.active.each do |t|
       t.collect_pledges
